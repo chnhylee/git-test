861d66c3d654ae176ed8073c7a2c98b093b0f7d6
chnhylee <36393744+chnhylee@users.noreply.github.com>
2021-06-02 13:29:34 +0800

    pull test

--- src/main/java/com/aeromu/git/GitTest.java
+++ src/main/java/com/aeromu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello Git!4");
         System.out.println("push test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
